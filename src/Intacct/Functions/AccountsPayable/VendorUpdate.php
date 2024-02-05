ca24317a2868af57d79b9fede68da17252adeb7b
WhistleTips <jaredlc@outlook.com>
2024-02-05 15:36:19 -0600

    fix: bad param

--- src/Intacct/Functions/AccountsPayable/VendorUpdate.php
+++ src/Intacct/Functions/AccountsPayable/VendorUpdate.php
@@ -67,7 +67,7 @@ class VendorUpdate extends AbstractVendor
         $xml->writeElement('EMAIL2', $this->getSecondaryEmailAddress());
         $xml->writeElement('URL1', $this->getPrimaryUrl());
         $xml->writeElement('URL2', $this->getSecondaryUrl());
-        $xml->writeElement('PO_REQUIRE', $this->getPoRequired());
+        $xml->writeElement('REQUIRE_PO', $this->getPoRequired());
 
         $this->writeXmlMailAddress($xml);
 
